e9727ab3ca06080e079d44eeacb936e99f8bdae3
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-06-09 23:30:48 +0200

    Initial commit

---
+++ src/main/java/net/kaupenjoe/tutorialmod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.kaupenjoe.tutorialmod.mixin;
+
+import net.kaupenjoe.tutorialmod.TutorialMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		TutorialMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
