95ac102036d9e5d27990771a3f5c803cbcd89432
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-06-16 23:19:50 +0200

    tooltips

--- src/main/java/net/kaupenjoe/tutorialmod/item/custom/EightBallItem.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/custom/EightBallItem.java
@@ -1,13 +1,19 @@
 package net.kaupenjoe.tutorialmod.item.custom;
 
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class EightBallItem extends Item {
     public EightBallItem(Settings settings) {
@@ -24,6 +30,17 @@ public class EightBallItem extends Item {
         }
 
         return super.use(world, user, hand);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if(Screen.hasShiftDown()) {
+            tooltip.add(Text.literal("Right Click to get a random number!").formatted(Formatting.AQUA));
+        } else {
+            tooltip.add(Text.literal("Press Shift for more info!").formatted(Formatting.YELLOW));
+        }
+
+        super.appendTooltip(stack, world, tooltip, context);
     }
 
     private void outputRandomNumber(PlayerEntity player) {
