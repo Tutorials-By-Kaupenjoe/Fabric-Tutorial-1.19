c25356db96fe8b5fa0ca82a8aac24834f1db4188
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-09-14 21:06:23 +0200

    better attack port

--- src/main/java/net/kaupenjoe/tutorialmod/entity/custom/ChomperEntity.java
+++ src/main/java/net/kaupenjoe/tutorialmod/entity/custom/ChomperEntity.java
@@ -1,24 +1,20 @@
 package net.kaupenjoe.tutorialmod.entity.custom;
 
-import net.kaupenjoe.tutorialmod.entity.ai.ChomperAttackGoal;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.data.DataTracker;
-import net.minecraft.entity.data.TrackedData;
-import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.passive.ChickenEntity;
-import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.passive.MerchantEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import software.bernie.geckolib3.core.AnimationState;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -29,9 +25,6 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 
 public class ChomperEntity extends HostileEntity implements IAnimatable {
     private AnimationFactory factory = new AnimationFactory(this);
-
-    private static final TrackedData<Boolean> ATTACKING =
-            DataTracker.registerData(ChomperEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
 
     public ChomperEntity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
@@ -48,7 +41,7 @@ public class ChomperEntity extends HostileEntity implements IAnimatable {
     @Override
     protected void initGoals() {
         this.goalSelector.add(1, new SwimGoal(this));
-        this.goalSelector.add(2, new ChomperAttackGoal(this, 1.2D, false));
+        this.goalSelector.add(2, new MeleeAttackGoal(this, 1.2D, false));
         this.goalSelector.add(3, new WanderAroundFarGoal(this, 0.75f, 1));
         this.goalSelector.add(4, new LookAroundGoal(this));
 
@@ -67,13 +60,14 @@ public class ChomperEntity extends HostileEntity implements IAnimatable {
         return PlayState.CONTINUE;
     }
 
-    private PlayState attackPredicate(AnimationEvent event) {
-        if(this.isAttacking()) {
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.chomper.attack", true));
-            return PlayState.CONTINUE;
+    private <E extends IAnimatable> PlayState attackPredicate(AnimationEvent<E> event) {
+        if (this.handSwinging && event.getController().getAnimationState().equals(AnimationState.Stopped)) {
+            event.getController().markNeedsReload();
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.chomper.attack", false));
+            this.handSwinging = false;
         }
 
-        return PlayState.STOP;
+        return PlayState.CONTINUE;
     }
 
     @Override
@@ -109,21 +103,5 @@ public class ChomperEntity extends HostileEntity implements IAnimatable {
     @Override
     protected void playStepSound(BlockPos pos, BlockState state) {
         this.playSound(SoundEvents.ENTITY_PIG_STEP, 0.15f, 1.0f);
-    }
-
-    @Override
-    public void setAttacking(boolean attacking) {
-        this.dataTracker.set(ATTACKING, attacking);
-    }
-
-    @Override
-    public boolean isAttacking() {
-        return this.dataTracker.get(ATTACKING);
-    }
-
-    @Override
-    protected void initDataTracker() {
-        super.initDataTracker();
-        this.dataTracker.startTracking(ATTACKING, false);
     }
 }
